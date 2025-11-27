2b5193c8e0454478685e6c95fdf20d14a2f9b8df
lukvmil <millerluke1364@gmail.com>
2025-11-26 20:59:45 -0500

    implementing cycle solver in assembler

--- src/koi_net/assembler.py
+++ src/koi_net/assembler.py
@@ -1,6 +1,7 @@
-from collections import deque
+from collections import defaultdict, deque
 import inspect
 from enum import StrEnum
+import math
 from pprint import pp
 from typing import Any, Protocol, Self
 from dataclasses import make_dataclass
@@ -22,22 +23,63 @@ class NodeContainer(Protocol):
     """Dummy 'shape' for node containers built by assembler."""
     entrypoint = EntryPoint
 
+def gname(group: tuple[int]):
+    return f"{len(group)}_{str(abs(hash(group)))[:5]}"
+
+def ledge(edges: tuple[tuple[str, str]]):
+    return ", ".join(f"({e[0]} -> {e[1]})" for e in edges)
+
 class NodeAssembler:
-    
-    
     # Self annotation lying to type checker to reflect typing set in node blueprints
     def __new__(self) -> Self:
         """Returns assembled node container."""
         
         comps = self._collect_comps()
-        # pp(list(comps.keys()))
         adj, comp_types = self._build_deps(comps)
-        # pp(adj)
-        # pp(comp_types)
         build_order = self._build_order(adj)
-        # pp(build_order)
+        
+        if len(build_order) != len(adj):
+            # cycles in dependency graph
+            node_seqs = self._find_cycles(adj)
+            cge_map = self._find_cycle_groups(node_seqs)
+            solutions = self._solve_cycles(cge_map)
+            
+            print(f"Found {len(solutions)} solutions:")
+            for edges in solutions:
+                print(", ".join(" -> ".join(edge) for edge in edges))
+            
+            uniq_solutions = {frozenset(sol) for sol in solutions}
+            
+            # cores = defaultdict(set)
+            # for i, s1 in enumerate(uniq_solutions):
+            #     for j, s2 in enumerate(uniq_solutions):
+            #         if i >= j:
+            #             continue
+                    
+            #         inter = s1 & s2
+            #         if len(inter) >= 2:
+            #             cores[frozenset(inter)].add(s1)
+            #             cores[frozenset(inter)].add(s2)
+            
+            edge_sol_map = {}
+            
+            print(len(uniq_solutions))
+            for sol in uniq_solutions:
+                print(sol)
+                # for edge in sol:
+                    
+            
+            # print()
+            # for intersection, solutions in cores.items():
+            #     print(ledge(intersection), "...", " OR ".join(ledge(s - intersection) for s in solutions))
+                
+            # breakpoint()
+            return solutions
+        
         components = self._build_comps(build_order, adj, comp_types)
         node = self._build_node(components)
+        
+        return node
         
         old = list(comps.keys())
         new = build_order
@@ -135,6 +177,145 @@ class NodeAssembler:
         return None
     
     @classmethod
+    def _find_cycles(cls, adj: dict[str, list[str]]) -> list[tuple[str]]:
+        cycles = []
+        for node in adj:
+            visited = {node: False for node in adj}
+            start = node
+            
+            def dfs(nodes):
+                head = nodes[-1]
+                
+                if visited[head]:
+                    if head == start:
+                        # check it is a unique cycle
+                        if all(
+                            # cycles different lengths
+                            len(nodes) != len(cycle_seq) or
+                            # cycles not equivalent
+                            "".join(nodes[:-1]) not in "".join(cycle_seq[:-1]) * 2
+                            for cycle_seq in cycles
+                        ):
+                            cycles.append(nodes)
+                    return
+            
+                visited[head] = True
+                for child in adj[head]:
+                    dfs(nodes + (child,))
+                    
+                visited[head] = False
+            
+            dfs((node,))
+        return cycles
+    
+    @classmethod
+    def _find_cycle_groups(cls, node_seqs: list[tuple[str]]):
+        # converts list of node sequences to edge sequences
+        # cycle # -> list of edges
+        edge_seqs = [
+            tuple(
+                (nodes[j], nodes[j+1])
+                for j in range(len(nodes) - 1)
+            )
+            for nodes in node_seqs
+        ]
+        
+        for i, edge_seq in enumerate(edge_seqs):
+            print(i, ":", ", ".join(" -> ".join(edge) for edge in edge_seq))
+        
+        # mapping of edge -> group of cycles it belongs to
+        edge_cycle_group_map: dict[tuple[str, str], list[int]] = {}
+        for i, edges in enumerate(edge_seqs):
+            for edge in edges:
+                edge_cycle_group_map.setdefault(edge, [])
+                edge_cycle_group_map[edge].append(i)
+        
+        cycle_group_edges_map: dict[tuple[int], list[frozenset[str, str]]] = {}
+        for edge, cycles in edge_cycle_group_map.items():
+            cycle_group = frozenset(cycles)
+            
+            cycle_group_edges_map.setdefault(cycle_group, [])
+            cycle_group_edges_map[cycle_group].append(edge)
+        
+        return cycle_group_edges_map
+    
+    @classmethod
+    def _solve_cycles(cls, cge_map: dict[frozenset[int], list[tuple[str, str]]]):
+        print("STARTING CYCLE SOLVER")
+        
+        solutions = []
+        min_solution = math.inf
+        def recurse(
+            cge_map: dict[frozenset[int], list[tuple[str, str]]],
+            removed_edges: tuple[tuple[str, str]] = ()
+        ):
+            cge_map = dict(sorted(
+                cge_map.items(),
+                key=lambda i: len(i[0]),
+                reverse=True
+            ))
+            
+            nonlocal min_solution
+            
+            # for group, edges in cge_map.items():
+            #     print(gname(group), ledge(edges))
+            
+            # quit()
+            
+            # print(f"There are {len(cge_map)} cycle group(s):\n")
+            
+            # max_cgs = []
+            # for group in cge_map.keys():
+            #     if max_cgs and len(group) < len(max_cgs[0]):
+            #         break
+                
+            #     max_cgs.append(group)
+                
+            # print(max_cgs)
+            
+            for max_group in cge_map:
+                for edge in cge_map[max_group]:
+                    next_removed_edges = removed_edges + (edge,)
+                    
+                    if len(next_removed_edges) > min_solution:
+                        # print("TOO LONG:", ledge(next_removed_edges))
+                        continue
+                    
+                    curr_cge_map = cge_map.copy()
+                    del curr_cge_map[max_group]
+                    
+                    next_cge_map: dict[frozenset[int], list[tuple[str, str]]] = {}
+                    for curr_group, edges in curr_cge_map.items():
+                        new_group = curr_group - max_group
+                        
+                        # print(",".join(map(str,curr_group)), "->", ",".join(map(str, new_group)))
+                        
+                        if not new_group:
+                            continue
+                        
+                        next_cge_map.setdefault(new_group, [])
+                        next_cge_map[new_group].extend(edges)
+                    
+                    # print(next_cge_map)
+                    
+                    # print(next_removed_edges)
+                    if next_cge_map:
+                        recurse(next_cge_map, next_removed_edges)
+                    else:
+                        if len(next_removed_edges) < min_solution:
+                            solutions.clear()
+                            solutions.append(next_removed_edges)
+                            min_solution = len(next_removed_edges)
+                            print(f"NEW MIN {min_solution}:", ledge(next_removed_edges))
+                        elif len(next_removed_edges) == min_solution:
+                            solutions.append(next_removed_edges)
+
+                            # print("EQUAL", ledge(next_removed_edges))
+                            
+        recurse(cge_map)
+        return solutions
+    
+    @classmethod
     def _build_order(cls, adj) -> list[str]:
         # adj list: n -> outgoing neighbors
         
@@ -204,7 +385,7 @@ class NodeAssembler:
     def _build_comps(
         cls,
         build_order: list[str],
-        dep_graph: dict[str, list[str]],
+        adj: dict[str, list[str]],
         comp_type: dict[str, CompType]
     ) -> dict[str, Any]:
         """Returns assembled components from dependency graph."""
@@ -220,7 +401,7 @@ class NodeAssembler:
             elif comp_type[comp_name] == CompType.FACTORY:
                 # builds depedency dict for current component
                 dependencies = {}
-                for dep in dep_graph[comp_name]:
+                for dep in adj[comp_name]:
                     if dep not in components:
                         raise Exception(f"Couldn't find required component '{dep}'")
                     dependencies[dep] = components[dep]
