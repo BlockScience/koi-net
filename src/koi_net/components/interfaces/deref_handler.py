b9498bcacf94a3adb3e669c13d80d5701dfcabec
lukvmil <millerluke1364@gmail.com>
2026-03-02 16:58:16 -0500

    patch: 1.3.1 - added log as default component on deref handlers, changed rid_types to a non init field to prevent dataclass errors

--- src/koi_net/components/interfaces/deref_handler.py
+++ src/koi_net/components/interfaces/deref_handler.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from logging import Logger
 from typing import TYPE_CHECKING
 
 from rid_lib.core import RID, RIDType
@@ -10,9 +11,10 @@ if TYPE_CHECKING:
 
 @dataclass
 class DerefHandler:
+    log: Logger
     effector: "Effector"
     
-    rid_types: tuple[RIDType] = ()
+    rid_types: tuple[RIDType] = field(init=False)
     
     def __post_init__(self):
         self.effector.register_handler(self)
